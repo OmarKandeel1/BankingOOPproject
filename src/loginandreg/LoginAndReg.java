61c5a8e3b01b318f38ffd420a7b8bbde804bea55
OmarKandeel <OmarKandeel@DESKTOP-5L2GM75>
2024-04-22 23:32:30 +0200

    First Commit to do

---
+++ src/loginandreg/LoginAndReg.java
@@ -0,0 +1,31 @@
+package loginandreg;
+
+import javax.swing.*;  
+import java.awt.*;  
+//class IconExample {   
+//IconExample(){   
+//JFrame f=new JFrame();   
+//Image icon = Toolkit.getDefaultToolkit().getImage("C:\\Users\\OmarKandeel\\Documents\\NetBeansProjects\\loginAndReg\\src\\Logo");  
+//f.setIconImage(icon);  
+//f.setLayout(null);   
+//f.setSize(200,200);   
+//f.setVisible(true);   
+//}   }
+
+public class LoginAndReg {
+
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Login LoginFrame = new Login();
+        LoginFrame.setVisible(true);
+        LoginFrame.pack();
+        LoginFrame.setLocationRelativeTo(null);
+
+//new ToolkitExample();   
+
+    }
+
+}
+
+ 
+       
